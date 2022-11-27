93995c26bf3952192fd36e400b09dcf67e30a20e
Pendramon <pendramon2@gmail.com>
2022-11-27 15:02:24 +0100

    Added DbContext, removed MongoContext

---
+++ Rogue.Contacts.Data/ContactsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Rogue.Contacts.Data.Model;
+
+namespace Rogue.Contacts.Data;
+
+public class ContactsContext : DbContext
+{
+    private readonly IConfiguration configuration;
+
+    public ContactsContext(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Business> Businesses => Set<Business>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(configuration["RogueContacts:ConnectionString"]);
+}
